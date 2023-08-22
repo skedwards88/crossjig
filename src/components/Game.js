caca25ea1c8a64f84f0ab8459386123d9b613d89
skedwards88 <skedwards88@github.com>
2023-08-22 08:55:41 -0700

    set transparent drag image so that dragging will work in safari

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,7 +13,13 @@ function Game({ dispatchGameState, gameState, setDisplay }) {
     boardTop, // the row on the board being dragged (undefined if dragging piece from pool)
     boardLeft, // the col on the board being dragged (undefined if dragging piece from pool)
   }) {
-    event.dataTransfer.setDragImage(document.createElement("img"), 0, 0);
+    let dragImage = new Image()
+    // 1x1 px transparent gif
+    // (Safari won't drag if there is an issue setting the drag image)
+    dragImage.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+    dragImage.style.visibility = 'hidden';
+    event.dataTransfer.setDragImage(dragImage, 0, 0);
+
     event.dataTransfer.setData(
       "draggedElementID",
       event.target.parentElement.id
