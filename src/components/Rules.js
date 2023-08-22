e56be629d4dfb30d4bc7b0ac05bd4c56eac8b098
skedwards88 <skedwards88@github.com>
2023-08-22 08:12:48 -0700

    add emphasis to rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,7 +5,31 @@ export default function Rules({ setDisplay }) {
   return (
     <div className="App rules">
       <h1 id="rulesHeader">Crossjig: How to play</h1>
-      <p id="rulesText">{`Arrange the pieces to make words vertically and horizontally. All words must connect.\n\nDrag a blank space to move the whole puzzle.\n\nLong press and drag to move a group of touching pieces.\n\nClick the magnifying glass to get a hint. A hint will move all pieces that are on the board to their correct location. If all pieces are already in the correct location, a new piece will be added to the board.\n\nClick the gear to change the number of pieces in the puzzle.\n\nClick the calendar to play the daily challenge. The daily challenge is easiest on Monday and gets harder over the week.`}</p>
+      <p id="rulesText">
+        <b>Drag</b> the pieces to the board to make words vertically and
+        horizontally. All words must connect.
+        <br />
+        <br />
+        <b>Drag a blank space</b> to move the whole puzzle.
+        <br />
+        <br />
+        <b>Long press and drag</b> to move a group of touching pieces.
+        <br />
+        <br />
+        Click the <b>magnifying glass</b> to get a hint. A hint will move all
+        pieces that are on the board to their correct location. If all pieces
+        are already in the correct location, a new piece will be added to the
+        board.
+        <br />
+        <br />
+        Click the <b>gear</b> to change the number of pieces in the puzzle.
+        <br />
+        <br />
+        Click the <b>calendar</b> to play the daily challenge. The daily
+        challenge is easiest on Monday and gets harder over the week.
+        <br />
+        <br />
+      </p>
       <button
         id="rulesClose"
         className="close"
