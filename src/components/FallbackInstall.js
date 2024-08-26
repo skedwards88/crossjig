cb6fcbc64bb0d1300bd038a011a7af6689bd11ac
skedwards88 <skedwards88@github.com>
2024-08-26 15:35:59 -0700

    update text

--- src/components/FallbackInstall.js
+++ src/components/FallbackInstall.js
@@ -4,7 +4,24 @@ export default function FallbackInstall({setDisplay}) {
   return (
     <div className="App info">
       <div className="infoText">
-        {"This app is a progressive web app, which means that it can be installed on your phone for easy access and offline play.\n\nFor Android, \n\nFor iOS, open Crossjig in Safari and select 'add to home screen' (under the Safari 'share' menu).)\n\n\n\nTo install, do a web search for how to install a progressive web app for your phone."}
+        <p>
+          Crossjig is a progressive web app, which means that it can be
+          installed on your phone for easy access and offline play.
+        </p>
+        <p>
+          {
+            "For iOS: Open Crossjig in Safari and select 'add to home screen' (under the Safari 'share' menu)."
+          }
+        </p>
+        <p>
+          {
+            "For Android: Open Crossjig in Chrome and select 'install' (under the Chrome ⋮ menu)."
+          }
+        </p>
+        <p>
+          For the most up-to-date instructions, do a web search for how to
+          install a progressive web app for your phone.
+        </p>
       </div>
       <button className="close" onClick={() => setDisplay("game")}>
         Close
