eabee82fac90b971a40723d70a7c3baa9200fc03
skedwards88 <skedwards88@github.com>
2024-09-07 09:54:10 -0700

    show images of more games

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./styles/ControlBar.css";
 import "./styles/Settings.css";
 import "./styles/Rules.css";
 import "./styles/Stats.css";
+import "./styles/MoreGames.css";
 import "./styles/LargeScreen.css";
 
 if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
