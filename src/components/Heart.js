3319e60b3c2cb02118e6b05e977836b05509354c
Sarah Edwards <skedwards88@gmail.com>
2025-08-10 09:44:17 -0700

    add privacy tl;dr

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -4,25 +4,28 @@ export default function Heart({setDisplay, repoName}) {
   return (
     <div className="App info">
       <div className="infoText">
-        {"Feedback? "}
-        <a
-          href={`https://github.com/skedwards88/${repoName}/issues/new/choose`}
-        >
-          Open an issue
-        </a>
-        {" on GitHub or email SECTgames@gmail.com."}
-        {`\n\n`}
-        {<hr></hr>}
-        {`\n`}
-        {`Thanks to the word frequency data sources attributed in `}
-        <a href="https://github.com/skedwards88/word_lists">
-          skedwards88/word_lists
-        </a>
-        {`.`}
-        {`\n\n`}
-        {<hr></hr>}
-        {`\n`}
-        <a href="./privacy.html">Privacy policy</a>
+        <p>
+          Feedback?{" "}
+          <a
+            href={`https://github.com/skedwards88/${repoName}/issues/new/choose`}
+          >
+            Open an issue
+          </a>{" "}
+          on GitHub or email SECTgames@gmail.com.
+        </p>
+        <hr></hr>
+        <p>
+          Thanks to the word frequency data sources attributed in{" "}
+          <a href="https://github.com/skedwards88/word_lists">
+            skedwards88/word_lists
+          </a>
+          .
+        </p>
+        <hr></hr>
+        <p>
+          <a href="./privacy.html">Privacy policy</a>
+        </p>
+        <small>tl;dr: We only collect anonymous data about usage.</small>
       </div>
       <button className="close" onClick={() => setDisplay("game")}>
         Close
